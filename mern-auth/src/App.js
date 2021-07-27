9d340d4d7780fdf6416184444bdd815ce7169da0
Jaydeep-Rajani-Dev <jaydeeprajani05@gmail.com>
2021-07-27 18:28:30 +0530

    Setup

---
+++ mern-auth/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+       
+      </header> */}
+      <h1>Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
